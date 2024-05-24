4a6f192427de5714255c2fb206cf1ecb4e163531
Faveero <159203076+Faveero@users.noreply.github.com>
2024-05-24 09:00:01 -0300

    acs

---
+++ main.js
@@ -0,0 +1,10 @@
+const perguntas = [ //abre uma lista de perguntas
+    { //abre objeto de pergunta
+        enunciado: "Pergunta 1",
+        alternativas: ["Alternativa 1", "Alternativa 2"],
+    },
+    {
+        enunciado: "Pergunta 2",
+        alternativas: ["Alternativa 1", "Alternativa 2"],
+    },
+]
