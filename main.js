697d4e3ccce70d5da20d3db99ba10481cf90d8c5
Faveero <159203076+Faveero@users.noreply.github.com>
2024-05-28 16:24:30 -0300

    ac

--- main.js
+++ main.js
@@ -1,3 +1,8 @@
+const caixaPrincipal = document.querySelector(".caixa-principal");
+const caixaPrincipal = document.querySelector(".caixa-perguntas");
+const caixaPrincipal = document.querySelector(".caixa-alternativas");
+const caixaPrincipal = document.querySelector(".caixa-resultado");
+const caixaPrincipal = document.querySelector(".texto-resultado");
 const perguntas = [ //abre uma lista de perguntas
     { //abre objeto de pergunta
         enunciado: "Pergunta 1",
@@ -6,5 +11,20 @@ const perguntas = [ //abre uma lista de perguntas
     {
         enunciado: "Pergunta 2",
         alternativas: ["Alternativa 1", "Alternativa 2"],
-    },
-]
+    }, 
+    {
+        enunciado: "Pergunta 3",
+        alternativas: ["Alternativa 1", "Alternativa 2"],
+    }, //fecha objeto de pergunta
+]; //fecha lista de pergunta
+
+let atual = 0;
+let perguntaAtual;
+
+function mostraPergunta(){
+    perguntaAtual = perguntas[atual];
+    caixaPerguntas.textContent = perguntaAtual.enunciado;
+}
+mostraPergunta();
+
+const caixaPerguntas = document.querySelector(".caixa-perguntas");
