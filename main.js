b3f73af09df9e2dec4fa13dfc11cf1e3296fa69e
Faveero <159203076+Faveero@users.noreply.github.com>
2024-06-14 08:16:18 -0300

    fe

--- main.js
+++ main.js
@@ -1,30 +1,30 @@
 const caixaPrincipal = document.querySelector(".caixa-principal");
-const caixaPrincipal = document.querySelector(".caixa-perguntas");
-const caixaPrincipal = document.querySelector(".caixa-alternativas");
-const caixaPrincipal = document.querySelector(".caixa-resultado");
-const caixaPrincipal = document.querySelector(".texto-resultado");
+const caixaPerguntas = document.querySelector(".caixa-perguntas");
+const caixaAlternativas = document.querySelector(".caixa-alternativas");
+const caixaResultado = document.querySelector(".caixa-resultado");
+const textoResultado = document.querySelector(".texto-resultado");
 const perguntas = [ //abre uma lista de perguntas
     { //abre objeto de pergunta
         enunciado: "Pergunta 1",
-        alternativas: ["Alternativa 1", "Alternativa 2"],
+        alternativas: ["Alternativa 1", "Alternativa 2"]
     },
     {
         enunciado: "Pergunta 2",
-        alternativas: ["Alternativa 1", "Alternativa 2"],
+        alternativas: ["Alternativa 1", "Alternativa 2"]
     }, 
     {
         enunciado: "Pergunta 3",
-        alternativas: ["Alternativa 1", "Alternativa 2"],
-    }, //fecha objeto de pergunta
+        alternativas: ["Alternativa 1", "Alternativa 2"]
+    } //fecha objeto de pergunta
 ]; //fecha lista de pergunta
 
-let atual = 0;
-let perguntaAtual;
+let atual = 0; //marcador de posição; começa pela pergunta 1
+let perguntaAtual; //variável que vai receber o texto
 
-function mostraPergunta(){
+function mostraPergunta(){ //função que faz aparecer a pergunta
     perguntaAtual = perguntas[atual];
     caixaPerguntas.textContent = perguntaAtual.enunciado;
 }
 mostraPergunta();
 
-const caixaPerguntas = document.querySelector(".caixa-perguntas");
+//const caixaPerguntas = document.querySelector(".caixa-perguntas");
