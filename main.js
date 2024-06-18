9e94758744842eb18ceca452887a3242d439bae7
Faveero <159203076+Faveero@users.noreply.github.com>
2024-06-17 22:56:02 -0300

    cas

--- main.js
+++ main.js
@@ -1,10 +1,10 @@
-// const: contante que guarda uma lista de objetos (perguntas, alternativas...)
-// dentro dos [] cada {} é um objeto
-const caixaPrincipal = document.querySelector(".caixa-principal");
-const caixaPerguntas = document.querySelector(".caixa-perguntas");
-const caixaAlternativas = document.querySelector(".caixa-alternativas");
-const caixaResultado = document.querySelector(".caixa-resultado");
-const textoResultado = document.querySelector(".texto-resultado");
+// const: constante que guarda uma lista de objetos (perguntas, alternativas...)
+// dentro dos [], cada {} é um objeto
+const caixaPrincipal = document.querySelector(".caixa-principal"); //seleciona a classe caixa-principal do HTML
+const caixaPerguntas = document.querySelector(".caixa-perguntas"); //seleciona a classe caixa-perguntas do HTML
+const caixaAlternativas = document.querySelector(".caixa-alternativas"); //seleciona a classe caixa-alternativas do HTML
+const caixaResultado = document.querySelector(".caixa-resultado"); //seleciona a classe caixa-resultado do HTML
+const textoResultado = document.querySelector(".texto-resultado"); //seleciona a classe texto-resultado do HTML
 
 const perguntas = [ //abre uma lista de perguntas
     { //abre objeto de pergunta
@@ -19,22 +19,40 @@ const perguntas = [ //abre uma lista de perguntas
             afirmacao: "Afirmação",
         },
     ]
-    },
-    {
+    }, //fecha objeto de pergunta
+    { //abre objeto de pergunta
         enunciado: "Pergunta 2",
-        alternativas: ["Alternativa 3", "Alternativa 4"]
-    }, 
-    {
+        alternativas: [
+        {
+            texto: "Que não sei que sei que lá",
+            afirmacao: "Afirmação"
+        },
+        {
+            texto: "Sei lá sei que",
+            afirmacao: "Afirmação",
+        },
+    ]
+    }, //fecha objeto de pergunta
+    { //abre objeto de pergunta
         enunciado: "Pergunta 3",
-        alternativas: ["Alternativa 5", "Alternativa 6"]
+        alternativas: [
+        {
+            texto: "Bla bla bla bla",
+            afirmacao: "Afirmação"
+        },
+        {
+            texto: "",
+            afirmacao: "Afirmação",
+        },
+    ]
     } //fecha objeto de pergunta
 ]; //fecha lista de pergunta
 
-let atual = 0; //marcador de posição; começa pela pergunta 1
+let atual = 0; //variável marcador de posição; começa pela pergunta 1
 let perguntaAtual; //variável que vai receber o texto
 
 function mostraPergunta(){ //função que faz aparecer a pergunta
-    perguntaAtual = perguntas[atual];
+    perguntaAtual = perguntas[atual]; //declara a variável
     caixaPerguntas.textContent = perguntaAtual.enunciado;
 }
 mostraPergunta();
