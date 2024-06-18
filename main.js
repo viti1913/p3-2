197e6f6e6abf53ca63400262a99b19a956b93b1e
Faveero <159203076+Faveero@users.noreply.github.com>
2024-06-17 22:27:03 -0300

    ge

--- main.js
+++ main.js
@@ -32,7 +32,11 @@ mostraPergunta();
 function mostraAlternativas(){
     for(const alternativa of perguntaAtual.alternativas){
         const botaoAlternativas = document.createElement("button");
-        botaoAlternativas.textContent = alternativa;
+        botaoAlternativas.textContent = alternativa.texto;
+        botaoAlternativas.addEventListener("click", function(){
+            atuall++;
+            mostraPergunta();
+        })
         caixaAlternativas.appendChild(botaoAlternativas);
     }
 }
